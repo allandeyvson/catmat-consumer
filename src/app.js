2dda6f97c61f88437ea008993a61985db38b2900
allan araujo <deyvson7@gmail.com>
2020-07-28 21:10:34 -0300

    page create to search engine

--- src/app.js
+++ src/app.js
@@ -1,13 +1,25 @@
-var express = require('express');
+const express = require('express');
+const db = require('./db/mongoDB')
+const schema = require('./db/materialSchema');
+const { mongo } = require('mongoose');
+
 
 function app() {
 
     function start() {
-        var app = express();
+        const app = express();
+        const mongoDB = db(schema)
+
         app.set('view engine', 'ejs');
 
-        app.get('/search', function (req, res) {
-            res.render("../view/index");
+        app.get('/', function (req, res) {
+            res.render("../view/index", {results : false});
+        });
+
+        app.get('/search', async function (req, res) {
+            var searchParams = req.query.query.toUpperCase().split(' ')
+            var result = await mongoDB.read(searchParams)
+            res.render("../view/index", {results : true, search : req.query.query, list: result});
         });
 
         app.listen(9000, function () {
