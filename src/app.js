74386c5473a0e9423ecbcae1fff82f1a395b93dc
allan araujo <deyvson7@gmail.com>
2020-08-28 16:27:39 -0300

    filter fixed in search engine

--- src/app.js
+++ src/app.js
@@ -14,20 +14,20 @@ function app() {
         app.set('view engine', 'ejs');
 
         app.get('/', function (req, res) {
-            res.render("../view/index", {results : false});
+            res.render("../view/index", {results : false, radioType : 'Material'});
         });
 
         app.get('/search', async function (req, res) {
             if (req.query.query !== undefined){
                 var searchParams = util.normalizeText(req.query.query)
-                                
+
                 var radioType = req.query.radioType
     
                 var result = await mongoDB.read(searchParams.split(' '), radioType)    
     
-                res.render("../view/index", {results : true, search : req.query.query, list: result});    
+                res.render("../view/index", {results : true, search : req.query.query, list: result, radioType:radioType});    
             } else {
-                res.render("../view/index", {results : false});
+                res.render("../view/index", {results : false, radioType : 'Material'});
             }
         });
 
